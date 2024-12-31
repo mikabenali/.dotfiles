ad7c7cadd00be234633e42614864b3e20ad24f4b
Michael <michael@nixos.home>
2024-12-31 15:15:21 +0100

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# set -x PATH  "$HOME/.config/emacs/bin" $PATH
+
+alias :q "exit"
+alias n "nvim"
+alias cl "clear"
+
+alias ll "eza -l --icons"
+alias lla "eza -la --icons"
+
+# Init Starship prompt
+starship init fish | source
+
+# Init Atuin for command history
+atuin init fish | source
