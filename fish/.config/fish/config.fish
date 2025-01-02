cf73c11ac0db4080bed01d758a38a41c76667997
Michael <michael@nixos.home>
2025-01-02 10:24:46 +0100

    Add doom emacs config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# set -x PATH  "$HOME/.config/emacs/bin" $PATH
+set -x PATH  "$HOME/.config/emacs/bin" $PATH
 
 alias :q "exit"
 alias n "nvim"
 alias cl "clear"
+alias e "emacs -nw"
 
 alias ll "eza -l --icons"
 alias lla "eza -la --icons"
