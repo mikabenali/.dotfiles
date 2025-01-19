81469808333471eaebb5d4a49d4a9f8006b57827
Michael B <mika.benali@gmail.com>
2025-01-19 20:40:01 +0100

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,24 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Path
 set -x PATH  "$HOME/.config/emacs/bin" $PATH
 
-alias install "sudo zypper in"
-alias search "zypper search"
-alias remove "sudo zypper remove"
-
+# Alias
+alias install "sudo pacman -S"
+alias remove "sudo pacman -R"
+alias search "pacman -F"
 alias :q "exit"
 alias n "nvim"
 alias cl "clear"
-
 alias e "emacsclient -nw"
-
 alias ll "eza -l --icons"
 alias lla "eza -la --icons"
-
 alias reload-hyprpaper "kill hyprpaper && hyprpaper &"
 alias reload-waybar "kill waybar && waybar &"
-
 alias shutdown "sudo /sbin/shutdown now"
 
 # Init Starship prompt
